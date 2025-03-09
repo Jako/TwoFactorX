da4591cbf9a7f9b67128fb2a0ed1b3af37bd478a
Thomas Jakobi <thomas.jakobi@partout.info>
2025-03-09 17:57:18 +0100

    Deleted translation using Weblate (Dutch)

--- core/components/twofactorx/lexicon/nl/permissions.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Permissions Lexicon Entries for TwoFactorX
- *
- * @package twofactorx
- * @subpackage lexicon
- */
-$_lang['twofactorx.permission.twofactorx_edit_desc'] = 'To update the TwoFactorX data in the user edit page.';
